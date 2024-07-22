9855db82b2f907c7127a9d2f7c4dec3d818702e1
ebihra <ebihara@example.com>
2024-07-22 02:56:19 +0000

    認証の「formデータを受け取る」所まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,16 +4,32 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginPostRequest;
 
 class AuthController extends Controller
 {
     /**
      * トップページを表示する
-     * 
+     *
      * @return \Illuminate\View\View
      */
      public function index()
      {
          return view('index');
+     }
+
+     /**
+      * ログイン処理
+      *
+      */
+     public function login(LoginPostRequest $request)
+     {
+        //  validate済
+
+        // データの取得
+        $datum = $request->validated();
+
+        //
+        var_dump($datum); exit;
      }
 }
