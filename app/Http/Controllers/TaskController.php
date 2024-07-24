3e2611e4e9db3a2df9d4b6fee3559347de0a5f89
ebihra <ebihara@example.com>
2024-07-24 10:35:25 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,12 +17,16 @@ class TaskController extends Controller
      */
      public function list()
      {
+         // 1Page辺りの表示アイテム数を設定
+        $per_page = 5;
+
         //  一覧の取得
         $list = TaskModel::where('user_id', Auth::id())
                         ->orderBy('priority', 'DESC')
                         ->orderBy('period')
                         ->orderBy('created_at')
-                        ->get();
+                        ->paginate($per_page);
+                        // ->get();
         /*
         $sql = TaskModel::where('user_id', Auth::id())
                         ->orderBy('priority', 'DESC')
