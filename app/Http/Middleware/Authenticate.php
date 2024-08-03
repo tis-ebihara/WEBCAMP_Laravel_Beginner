2280b6b9a712ab1d349da5802eaacdc3a76f04f9
ebihra <ebihara@example.com>
2024-08-03 16:59:23 +0000

    管理画面の認証と認可を書いた

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,8 +12,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        if (! $request->expectsJson()){
-            return route('front.index');
+        if (! $request->expectsJson()) {
+            if (0 === strncasecmp($request->path(), 'admin/', strlen('admin/'))) {
+                return route('admin.index');
+            } else {
+                return route('front.index');
+            }
         }
     }
 }
